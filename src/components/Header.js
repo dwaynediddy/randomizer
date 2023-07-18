5181006ba51427b75263201c895609485df4c070
dwaynediddy <burrows.dwayne@icloud.com>
2023-07-18 16:58:21 +0200

    added new items

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,36 @@
-import React from 'react'
+import { useState } from 'react'
 import './style/header.css'
 
 const Header = () => {
+    const [activeButton, setActiveButton] = useState(null)
+
+    const handleButtonClick = (button) => {
+        setActiveButton(button)
+    }
+
   return (
     <div>
-        <h2>Select your Path</h2>
-        <button>Raid upgrade</button>
-        <button>Team challange</button>
-        <button>Grind Challange</button>
+      <h2>Select your Path</h2>
+      <div className="button-container">
+        <button
+          className={activeButton === 'Raid upgrade' ? 'active' : ''}
+          onClick={() => handleButtonClick('Raid upgrade')}
+        >
+          Raid upgrade
+        </button>
+        <button
+          className={activeButton === 'Team challange' ? 'active' : ''}
+          onClick={() => handleButtonClick('Team challange')}
+        >
+          Team challange
+        </button>
+        <button
+          className={activeButton === 'Grind Challange' ? 'active' : ''}
+          onClick={() => handleButtonClick('Grind Challange')}
+        >
+          Grind Challange
+        </button>
+      </div>
     </div>
   )
 }
--- src/components/randomizer.js
+++ src/components/randomizer.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 
+// toa images
 import fang from '../assets/fang.png'
 import lightbearer from '../assets/lightbearer.png'
 import mask from '../assets/mask.png'
@@ -8,8 +9,24 @@ import mchaps from '../assets/mchaps.png'
 import ward from '../assets/ward.png'
 import shadow from '../assets/shadow.png'
 
+// cox images
+import dex from '../assets/dex.png'
+import arcane from '../assets/arcane.png'
+import ahat from '../assets/ahat.png'
+import atop from '../assets/atop.png'
+import alegs from '../assets/alegs.png'
+import buckler from '../assets/buckler.png'
+import dhcb from '../assets/dhcb.png'
+import dinz from '../assets/dinz.png'
+import kodai from '../assets/kodai.png'
+import claws from '../assets/claws.png'
+import maul from '../assets/maul.png'
+import tbow from '../assets/tbow.png'
+
 const Randomizer = () => {
-    const images = [fang, lightbearer, mask, mbody, mchaps, shadow, ward]
+    const toaImages = [fang, lightbearer, mask, mbody, mchaps, shadow, ward]
+
+    const coxImages = [dex, arcane, ahat, atop, alegs, buckler, dhcb, dinz, kodai, claws, maul, tbow]
     
     // const [selectedImages, setSelectedImages] = useState(null)
 
@@ -17,11 +34,19 @@ const Randomizer = () => {
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length)
+            setCurrentImageIndex((prevIndex) => (prevIndex + 1) % toaImages.length)
         }, 500)
 
         return () => clearInterval(interval)
-    },[images])
+    },[toaImages])
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentImageIndex((prevIndex) => (prevIndex + 1) % coxImages.length)
+        }, 500)
+
+        return () => clearInterval(interval)
+    },[coxImages])
 
     // randomly select an image from the images array on button click
     // const handleRandomizer = () => {
@@ -31,7 +56,8 @@ const Randomizer = () => {
 
   return (
     <div>
-        {<img src={images[currentImageIndex]} alt="Flickering" />}
+        {<img src={toaImages[currentImageIndex]} alt="Flickering" />}
+        {<img src={coxImages[currentImageIndex]} alt="Flickering" />}
         {/* //  button that randomly displays an image from the images array 
         <button onClick={handleRandomizer} />
         {selectedImages && <img src={selectedImages} alt={selectedImages} />} */}
